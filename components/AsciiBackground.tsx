1bcd681c4e63ea0e3f74e46fcc76fc93e922170b
frodoloverings <33843390+frodoloverings@users.noreply.github.com>
2025-09-29 03:26:12 +0300

    Update AsciiBackground.tsx

--- components/AsciiBackground.tsx
+++ components/AsciiBackground.tsx
@@ -8,7 +8,7 @@ const CONFIG_BASE = {
   heatForce: 1.0,
   fadePerSec: 1.75,
   spawnJitter: 0.2,
-  hoverStart: '#fafafa',
+  hoverStart: '#33FF00',
   hoverEnd: '#383838',
   dotsColor: '#404040',
   fontScale: 0.31,
